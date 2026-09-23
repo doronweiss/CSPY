cd984839aa0179c753d582c5c360869c1f9d2b64
doron weiss <doron@10fs.co.il>
2026-09-23 16:16:09 +0300

    fixed to work on laptop

--- CSTester/CSTester/Program.cs
+++ CSTester/CSTester/Program.cs
@@ -11,7 +11,15 @@ internal class Program {
             // Update this string with your exact Python version and path!
             // Windows example: @"C:\Users\<User>\AppData\Local\Programs\Python\Python311\python311.dll"
             // Mac/Linux example: "/usr/local/lib/libpython3.11.dylib" or "libpython3.11.so"
-            Runtime.PythonDLL = @"c:\Users\USER\AppData\Local\Python\pythoncore-3.14-64\python314.dll";
+            Console.Write("enter 1 for desktop, 2 for laptop: ");
+            string choice = Console.ReadLine();
+            int option = int.Parse(choice);
+            Runtime.PythonDLL = option switch
+            {
+                1 => @"c:\Users\USER\AppData\Local\Python\pythoncore-3.14-64\python314.dll",
+                2 => @"c:\Users\doron\AppData\Local\Python\pythoncore-3.14-64\python314.dll",
+                _ => throw new ArgumentOutOfRangeException("Invalid option")
+            };
 
             // 2. Resolve paths for the python folder
             // We find 'pyfolder' assuming it lives parallel to or outside your executable's execution directory
@@ -59,7 +67,5 @@ internal class Program {
                 // 9. Shutdown the engine cleanly when your application ends
                 PythonEngine.Shutdown();
             }
-
-            Console.ReadLine();
         }
 }
